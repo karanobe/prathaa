6f8f7fdc8569415fca25fae98ef6dd3b4fcc5861
Karan Oberoi <koberoi1017@gmail.com>
2017-09-06 20:02:58 -0700

    Add initial links, content, and bootstrap

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,19 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/in-the-news' do
+  erb :"in_the_news"
+end
+
+get '/contact' do
+  erb :contact
+end
+
+get '/gallery' do
+  erb :gallery
 end
